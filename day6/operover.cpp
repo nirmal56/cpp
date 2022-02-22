f26a7c9145079f16f7463c88bab469928d5d3faf
nirmal56 <nirmal.patel@einfochips.com>
2022-02-22 12:07:41 +0530

    operator overloading added

---
+++ day6/operover.cpp
@@ -0,0 +1,46 @@
+//demonstrating operatoor over loading//
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class complex{
+	int real,img;
+public:
+	complex(){					//default const
+		real=0;
+		img=0;
+	}
+
+	complex(int x,int y){		//para const
+		real = x;
+		img = y;
+	}
+
+	void display(){				//display
+		cout<<"===> "<<real<<"+i"<<img<<endl;
+	}
+//only one parameter that is to be after the operatior in functionality.
+//first is before operator , seconde is after operator.
+	complex operator + (complex obj){			
+		complex ans;
+		ans.real = real + obj.real;
+		ans.img = img + obj.img;
+		return ans;
+	}
+
+};
+
+int main(){
+	complex c1(1,2),c2(1,2);
+	complex c3;
+	c1.display();
+	c2.display();
+	c3.display();
+
+	cout<< "action starts here:";
+	c3=c1+c2;
+	c3.display();
+
+	return 0;
+}
