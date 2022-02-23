781503e9c022ad788e1033a216e841979b2ffbc3
nirmal56 <nirmal.patel@einfochips.com>
2022-02-24 00:29:19 +0530

    added unary friend

---
+++ day6/unfrnd.cpp
@@ -0,0 +1,91 @@
+#include<iostream>
+
+using namespace std;
+
+class space{
+int x,y,z;
+public:
+	space(){
+		x=y=z=0;
+	}
+	space(int a,int b,int c){
+		x=a;
+		y=b;
+		z=c;
+	}
+
+	void display(){
+		cout<<"====>x="<<x<<",y="<<y<<",z="<<z<<endl;
+	}
+	friend space operator -();
+	friend space operator --();
+	friend space operator ++();
+	friend space operator ++(int);
+	frie  space operator --(int);
+
+};
+
+ space operator -(){
+	space temp;
+	cout<<"\nnegate"<<endl;
+	temp.x=-x;
+	temp.y=-y;
+	temp.z=-z;
+}
+ space operator --(){
+	space temp;
+	cout<<"\npre decre";
+	temp.x=--x;
+	temp.y=--y;
+	temp.z=--z;
+	return temp;
+}
+ space operator ++(){
+	space temp;
+	cout<<"\npre incre";
+	temp.x=++x;
+	temp.y=++y;
+	temp.z=++z;
+	return temp;
+}
+ space operator --(int){
+	space temp;
+	cout<<"\npost decre";
+	temp.x=x--;
+	temp.y=y--;
+	temp.z=z--;
+	return temp;
+}
+ space operator ++(int){
+	space temp;
+	cout<<"\npost incre";
+	temp.x=x++;
+	temp.y=y++;
+	temp.z=z++;
+	return temp;
+}
+
+int main(){
+	space s1(5,4,3),d;
+
+	s1.display();
+
+	d=-s1;
+	s1.display();
+
+	d=--s1;
+	s1.display();
+
+	d=++s1;
+	s1.display();
+
+	d = s1++;
+	s1.display();
+
+	d=s1++;
+	s1.display();
+
+	d= -s1;
+	s1.display();
+	return 0;
+}
