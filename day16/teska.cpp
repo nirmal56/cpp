1fe47ec1fe78d514eab517196ec2637ab0eaf8da
nirmal56 <nirmal.patel@einfochips.com>
2022-03-16 10:23:51 +0530

    task added

---
+++ day16/teska.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<fstream>
+
+using namespace std;
+
+int main(){
+    ifstream file;
+    char ch;
+    int ws_count;
+
+    file.open("sample.txt");
+
+    if(!file){
+        cout<<"cannot open the file"<<endl;
+    }
+    else{
+        while(!file.eof()) {
+            file.get(ch);
+            if(ch==' '){
+                ws_count++;
+            }
+        }
+    }
+    cout<<"number of while spaces: "<<ws_count<<endl;
+    return 0;
+}
