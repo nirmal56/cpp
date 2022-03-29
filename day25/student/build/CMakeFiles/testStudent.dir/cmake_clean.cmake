b9006eb3fc8646d9a7c924e1f893a555f3330513
nirmal56 <nirmal.patel@einfochips.com>
2022-03-29 12:11:34 +0530

    day25 - camke

---
+++ day25/student/build/CMakeFiles/testStudent.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testStudent.dir/src/Student.cpp.o"
+  "CMakeFiles/testStudent.dir/src/mainapp.cpp.o"
+  "testStudent.pdb"
+  "testStudent"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/testStudent.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
